9833332ba283f90064803efde9868a776b8c6550
Pierre Obregon <pierreobregon@prima.com.pe>
2025-01-09 13:08:55 -0500

    jasencios: Inicia Escaneo de Dependencias Kuntur 17 - OCI

---
+++ src/main/java/com/prima/kuntur/oci/KunturOciApplication.java
@@ -0,0 +1,13 @@
+package com.prima.kuntur.oci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunturOciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KunturOciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prima/kuntur/oci/KunturOciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prima.kuntur.oci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunturOciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
